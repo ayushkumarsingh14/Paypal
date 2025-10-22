0683334e1d4e3c182de6817988620c17c1634571
Ayush <aaks.feb14@gmail.com>
2025-10-22 08:41:07 +0000

    user service

---
+++ user-service/src/main/java/com/paypal/user_service/service/UserServiceImp.java
@@ -0,0 +1,34 @@
+package com.paypal.user_service.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.paypal.user_service.entity.User;
+import com.paypal.user_service.repository.UserRepo;
+
+public class UserServiceImp implements UserService {
+
+    private UserRepo userRepo;
+
+    public UserServiceImp (UserRepo userRepo){
+        this.userRepo = userRepo;
+    }
+
+
+
+    @Override
+    public User createUser(User user) {
+        return (User) userRepo.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepo.findById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+    
+}
